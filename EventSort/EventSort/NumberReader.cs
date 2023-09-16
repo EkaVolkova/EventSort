83d92aa1a338f00e7fc66c558e7baa72b78a180a
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-16 22:52:06 +0500

    Создайте консольное приложение, в котором будет происходить сортировка списка фамилий из пяти человек. Сортировка должна происходить при помощи события. Сортировка происходит при введении пользователем либо числа 1 (сортировка А-Я), либо числа 2 (сортировка Я-А).
    Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally с использованием собственного типа исключения.

---
+++ EventSort/EventSort/NumberReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>
+/// Создайте консольное приложение, в котором будет происходить сортировка списка фамилий из пяти человек. 
+/// Сортировка должна происходить при помощи события. Сортировка происходит при введении пользователем либо числа 1 (сортировка А-Я), либо числа 2 (сортировка Я-А).
+/// Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally с использованием собственного типа исключения.
+/// </summary>
+namespace EventSort
+{
+    class NumberReader
+    {
+        //Делегат для оповещении о наступлении события
+        public delegate void NumberEnteredDelegate(int num);
+        //Событие ввода пользователем способа сортировки
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        /// <summary>
+        /// Считывание давннх о введенном пользователем методе сортировки
+        /// </summary>
+        public void Read()
+        {
+            Console.WriteLine("Введите число 1 (сортировка А-Я) или число 2 (сортировка Я-А)");
+            
+            //Считываем из консоли сивол
+            int num = int.Parse(Console.ReadLine());
+
+            //проверяем, что было введено 1 или 2
+            if (num != 1 && num != 2)
+                throw new EnteredException();
+
+            //Вызываем оповещение о событии
+            NumberEntered(num);
+
+        }
+
+        /// <summary>
+        /// Метод, вызывающий событие
+        /// </summary>
+        /// <param name="num">способ сортировки</param>
+        private void NumberEntered(int num)
+        {
+            NumberEnteredEvent?.Invoke(num);
+        }
+    }
+}
---
+++ EventSort/EventSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Создайте консольное приложение, в котором будет происходить сортировка списка фамилий из пяти человек. 
+/// Сортировка должна происходить при помощи события. Сортировка происходит при введении пользователем либо числа 1 (сортировка А-Я), либо числа 2 (сортировка Я-А).
+/// Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally с использованием собственного типа исключения.
+/// </summary>
+namespace EventSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем экземпляр класса NumberReader
+            NumberReader numberReader = new NumberReader();
+            //Добавляем обработчик события
+            numberReader.NumberEnteredEvent += SortArray;
+            try
+            {
+                //Считываем данные
+                numberReader.Read();
+            }
+            catch (Exception ex)
+            {
+
+                //Метод Read скорее всего вернет FormatException, но может вернуть и OverflowException
+                Console.WriteLine("Ошибка ввода: {0}", ex.Message);
+            }
+        }
+        /// <summary>
+        /// Сортирует массив по выбранному методу сортировки 
+        /// </summary>
+        /// <param name="arrayWay">Метод сортировки: 1 (сортировка А-Я), сортировка Я-А)</param>
+        static void SortArray(int arrayWay)
+        {
+            string[] words = { "the", "quick", "brown", "fox", "jumps" };
+            IEnumerable<string> sortWords;
+            if (arrayWay == 1)
+            {
+                sortWords = from word in words
+                            orderby word.Substring(0, 1)
+                            select word;
+            }
+            else
+            {
+                sortWords = from word in words
+                            orderby word.Substring(0, 1) descending
+                            select word;
+            }
+
+            foreach (var word in sortWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
